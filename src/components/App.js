4d3e315efecb2706264909036a650632f456c3c4
Karan09542 <karan09542@gmail.com>
2025-11-20 16:37:22 +0530

    creating tooltip

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,24 @@
 import React from "react";
 import './../styles/App.css';
 import Tab from "./Tab";
+import Tooltip from "./Tooltip";
 
 const App = () => {
-  const tabs = [
-    {title:"Tab 1", content: "Tab one"},
-    {title:"Tab 2", content: "Tab one"},
-    {title:"Tab 3", content: "Tab one"}
-  ]
+  // const tabs = [
+  //   {title:"Tab 1", content: "Tab one"},
+  //   {title:"Tab 2", content: "Tab one"},
+  //   {title:"Tab 3", content: "Tab one"}
+  // ]
   return (
     <div>
         {/* Do not remove the main div */}
-        <Tab tabs={tabs}/>
+        {/* <Tab tabs={tabs}/> */}
+        <Tooltip tooltiptext={"Thank you"}>
+          <p className="tooltip">Hover over me</p>
+        </Tooltip>
+        <Tooltip tooltiptext={"Thank you again"}>
+          <p className="tooltip">Hover over me once more</p>
+        </Tooltip>
     </div>
   )
 }
